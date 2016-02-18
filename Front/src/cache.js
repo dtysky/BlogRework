18a4c01655c8a3dbaa1663b4cf2306b7d7f5b95f
DaiTianyu <dtysky@outlook.com>
2016-02-18 14:02:43 +0800

    Add 'Access control allow'.

---
+++ Front/src/cache.js
@@ -0,0 +1,22 @@
+/**
+ * Created by dtysky on 16/2/17.
+ */
+"use strict";
+
+function Cache(){
+    this.pages = {};
+}
+
+Cache.prototype.add = function(name, content){
+    this.pages[name] = content;
+};
+
+Cache.prototype.has = function(name){
+    return this.pages[name] !== undefined;
+};
+
+Cache.prototype.get = function(name){
+    return this.pages[name];
+};
+
+module.exports = new Cache();
